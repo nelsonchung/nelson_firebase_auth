879ac972f574132a1f7c10aa0fe76530eff8f5e9
Nelson Chung <chihchun.chung@gmail.com>
2023-07-21 06:21:25 +0800

    UI設定功能完成。背景調整成淡藍到深藍的漸層。接下來要加上firebase_core的功能

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:logger/logger.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -8,40 +10,92 @@ void main() {
 }
 
 class LoginPage extends StatelessWidget {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  final Logger logger = Logger();
+
+  LoginPage({Key? key}) : super(key: key);
+
+  Future<void> signInWithEmailAndPassword(BuildContext context) async {
+    try {
+      UserCredential userCredential =
+          await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: emailController.text,
+        password: passwordController.text,
+      );
+      // 登入成功後的處理邏輯
+      logger.i('使用者登入成功：${userCredential.user!.uid}');
+    } catch (e) {
+      // 登入失敗的處理邏輯
+      logger.e('使用者登入失敗：$e');
+    }
+  }
+
+  Future<void> sendEmailVerification(BuildContext context) async {
+    User? user = FirebaseAuth.instance.currentUser;
+    if (user != null && !user.emailVerified) {
+      await user.sendEmailVerification();
+      // 寄送驗證郵件後的處理邏輯
+      logger.i('已寄送驗證郵件至使用者的信箱');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            FlutterLogo(
-              size: 100.0,
+      body: SingleChildScrollView(
+        child: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Colors.lightBlue,
+                Color.fromARGB(255, 2, 50, 90),
+              ],
             ),
-            SizedBox(height: 30.0),
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: '使用者名稱',
-                border: OutlineInputBorder(),
+          ),
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const FlutterLogo(
+                size: 100.0,
               ),
-            ),
-            SizedBox(height: 15.0),
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: '密碼',
-                border: OutlineInputBorder(),
+              const SizedBox(height: 30.0),
+              TextFormField(
+                controller: emailController,
+                decoration: const InputDecoration(
+                  labelText: '使用者名稱',
+                  border: OutlineInputBorder(),
+                ),
               ),
-              obscureText: true,
-            ),
-            SizedBox(height: 30.0),
-            ElevatedButton(
-              onPressed: () {
-                // 登入按鈕的點擊處理邏輯
-              },
-              child: Text('登入'),
-            ),
-          ],
+              const SizedBox(height: 15.0),
+              TextFormField(
+                controller: passwordController,
+                decoration: const InputDecoration(
+                  labelText: '密碼',
+                  border: OutlineInputBorder(),
+                ),
+                obscureText: true,
+              ),
+              const SizedBox(height: 30.0),
+              ElevatedButton(
+                onPressed: () {
+                  signInWithEmailAndPassword(context);
+                },
+                child: const Text('登入'),
+              ),
+              const SizedBox(height: 10.0),
+              ElevatedButton(
+                onPressed: () {
+                  sendEmailVerification(context);
+                },
+                child: const Text('寄送驗證郵件'),
+              ),
+            ],
+          ),
         ),
       ),
     );
