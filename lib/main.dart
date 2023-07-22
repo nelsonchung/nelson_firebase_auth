8756d64055cffb3cfb71d7a16a0c6bfcf4c51492
Nelson Chung <chihchun.chung@gmail.com>
2023-07-22 14:50:14 +0800

    參考firebase的網頁 https://firebase.google.com/docs/flutter/setup?hl=zh-tw&platform=ios
    透過firebase cli，完成相關設定。

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'firebase_options.dart';
 import 'package:logger/logger.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MaterialApp(
     title: '使用者登入',
     home: LoginPage(),
